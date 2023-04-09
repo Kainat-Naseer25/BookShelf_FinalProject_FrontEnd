6141b009fdaf1276b9803809bfa35e45c6db7ee1
katty-786 <naseerkainat202@gmail.com>
2023-04-09 16:34:19 +0500

    Complete card design

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import "./App.css";
+import BooksCard from "./Card/Card";
+import ViewDescription from "./ViewDescription/ViewDescription";
 import Authentication from "./Authentication/Authentication";
 import SignInForm from "./Authentication/SignInForm.js";
 import SignUpForm from "./Authentication/SignUpForm.js";
@@ -8,15 +10,17 @@ import Dashboard from "./Dashboard.js";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/auth" element={<Authentication />} >
-          <Route path="signin" element={<SignInForm />} />
-          <Route path="signup" element={<SignUpForm />} />
-        </Route>
-        <Route path="/" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
+      <ViewDescription/>
+
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/auth" element={<Authentication />} >
+    //       <Route path="signin" element={<SignInForm />} />
+    //       <Route path="signup" element={<SignUpForm />} />
+    //     </Route>
+    //     <Route path="/" element={<Dashboard />} />
+    //   </Routes>
+    // </BrowserRouter>
   );
 }
 
