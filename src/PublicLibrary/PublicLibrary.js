5cf2e9a5e5cda43d01ec96252c1744856fc88252
Maimoona Abid <maimoonaabid2000@gmail.com>
2023-04-12 19:51:07 +0500

    Update API and public library retrieve

--- src/PublicLibrary/PublicLibrary.js
+++ src/PublicLibrary/PublicLibrary.js
@@ -12,65 +12,61 @@ import ViewDescription from '../ViewDescription/ViewDescription';
 import { useDispatch, useSelector } from 'react-redux';
 
 const items = [
-    {
-        src: slide1,
-        caption: '"Books are a uniquely portable Magic"'
-    },
-    {
-        src: slide2,
-        caption: '"Today a Reader, tomorrow a Leader"'
-    },
-    {
-        src: slide3,
-        caption: '"There is no Friend as Loyal as Book"'
-    },
-    {
-        src: slide4,
-        caption: '"Happiness is reading Books at Night"'
-    }
+  {
+    src: slide1,
+    caption: '"Books are a uniquely portable Magic"',
+  },
+  {
+    src: slide2,
+    caption: '"Today a Reader, tomorrow a Leader"',
+  },
+  {
+    src: slide3,
+    caption: '"There is no Friend as Loyal as Book"',
+  },
+  {
+    src: slide4,
+    caption: '"Happiness is reading Books at Night"',
+  },
 ];
 
 const PublicLibrary = () => {
-    const [activeIndex, setActiveIndex] = useState(0);
-    const [animating, setAnimating] = useState(false);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
 
-    const onExiting = () => {
-        setAnimating(true);
-    }
+  const onExiting = () => {
+    setAnimating(true);
+  };
 
-    const onExited = () => {
-        setAnimating(false);
-    }
+  const onExited = () => {
+    setAnimating(false);
+  };
 
-    const next = () => {
-        if (animating) return;
-        const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
-        setActiveIndex(nextIndex);
-    }
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  };
 
-    const previous = () => {
-        if (animating) return;
-        const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
-        setActiveIndex(nextIndex);
-    }
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  };
 
-    const goToIndex = (newIndex) => {
-        if (animating) return;
-        setActiveIndex(newIndex);
-    }
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  };
 
-    const slides = items.map((item) => {
-        return (
-            <CarouselItem
-                onExiting={onExiting}
-                onExited={onExited}
-                key={item.src}
-            >
-                <img src={item.src} alt={item.altText} className="images" />
-                <CarouselCaption captionText={item.caption} />
-            </CarouselItem>
-        );
-    });
+  const slides = items.map((item) => {
+    return (
+      <CarouselItem onExiting={onExiting} onExited={onExited} key={item.src}>
+        <img src={item.src} alt={item.altText} className="images"  />
+        <CarouselCaption captionText={item.caption} />
+      </CarouselItem>
+    );
+  });
     const dispatch = useDispatch();
     const { descriptionModal, cdata } = useSelector(
         (state) => ({
@@ -92,34 +88,17 @@ const PublicLibrary = () => {
     if (error) return `An error has occurred: ${error.message}`;
 
     return (
-        <div className="dashboard">
-            <Carousel
-                activeIndex={activeIndex}
-                next={next}
-                previous={previous}
-                interval={5000}
-            >
-                <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
-                {slides}
-                <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
-                <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
-            </Carousel>
-
-            {data && data.length === 0 && <p>Currently No Books in Public Library</p>}
-            <CardGroup>
-                <Row className="mainRow">
-                    {data.map((key, index) => (
-                        <BooksCard
-                            className="column mb-5"
-                            key={index}
-                            item={key}
-                            data={data}
-                        />
-                    ))}
-                    ;{descriptionModal && <ViewDescription data={cdata} />}
-                </Row>
-            </CardGroup>
-        </div>
+        <Carousel
+            activeIndex={activeIndex}
+            next={next}
+            previous={previous}
+            interval={5000}
+        >
+            <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
+            {slides}
+            <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
+            <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
+        </Carousel>
     );
 }
 
